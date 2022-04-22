82b23c9fbf0fc8bde71d75307b03d43f7027e9b1
Operator Beamline Xaloc <opbl13@pcbl1307.cells.es>
2022-04-22 19:18:06 +0200

    Correcion of qled modules for use in the shutter state lights

--- mxcubeqt/utils/qled/qled_resources.py
+++ mxcubeqt/utils/qled/qled_resources.py
@@ -5,16 +5,16 @@ from mxcubeqt.utils import qt_import
 
 if qt_import.qt_variant == "PyQt4":
     if sys.version_info[0] == 3:
-        from mxcubeqt.utils.QLed.qled_resources_qt4_py3 import *
+        from mxcubeqt.utils.qled.qled_resources_qt4_py3 import *
     else:
-        from mxcubeqt.utils.QLed.qled_resources_qt4 import *
+        from mxcubeqt.utils.qled.qled_resources_qt4 import *
 elif qt_import.qt_variant == "PyQt5":
     if sys.version_info[0] == 3:
-        from mxcubeqt.utils.QLed.qled_resources_qt5_py3 import *
+        from mxcubeqt.utils.qled.qled_resources_qt5_py3 import *
     else:
-        from mxcubeqt.utils.QLed.qled_resources_qt5 import *
+        from mxcubeqt.utils.qled.qled_resources_qt5 import *
 elif qt_import.qt_variant == "PySide":
     if sys.version_info[0] == 3:
-        from mxcubeqt.utils.QLed.qled_resources_pyside_py3 import *
+        from mxcubeqt.utils.qled.qled_resources_pyside_py3 import *
     else:
-        from mxcubeqt.utils.QLed.qled_resources_pyside import *
+        from mxcubeqt.utils.qled.qled_resources_pyside import *
